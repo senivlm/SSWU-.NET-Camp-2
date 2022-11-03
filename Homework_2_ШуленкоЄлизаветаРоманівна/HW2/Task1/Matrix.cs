e51731bfbd0c9ff2e3123e0d8ccd33c60cedb894
ElizabethShulenko <lizashulenko@gmail.com>
2022-11-03 17:48:52 +0200

    update hw2

--- Homework_2_ШуленкоЄлизаветаРоманівна/HW2/Task1/Matrix.cs
+++ Homework_2_ШуленкоЄлизаветаРоманівна/HW2/Task1/Matrix.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Task1
+﻿namespace Task1
 {
     static internal class Matrix
     {
@@ -25,6 +19,36 @@ namespace Task1
                 }
                 else
                 {
+                    for (int rowIndex = rowCount - 1; rowIndex >= 0; rowIndex--)
+                    {
+                        array[rowIndex, columnIndex] = firstNum;
+
+                        firstNum++;
+                    }
+                }
+            }
+
+            return array;
+        }
+
+        public static int[,] CreateSnakedMatrixReverse(int rowCount, int columnCount, int firstNum)
+        {
+            int[,] array = new int[rowCount, columnCount];
+
+            for (int columnIndex = columnCount - 1; columnIndex >= 0; columnIndex--)
+            {
+                if (columnIndex % 2 == 0)
+                {
+                    for (int rowIndex = 0; rowIndex < rowCount; rowIndex++)
+                    {
+                        array[rowIndex, columnIndex] = firstNum;
+
+                        firstNum++;
+                    }
+                }
+                else
+                {
+
                     for (int rowIndex = rowCount - 1; rowIndex >= 0; rowIndex--)
                     {
                         array[rowIndex, columnIndex] = firstNum;
@@ -85,6 +109,53 @@ namespace Task1
             return array;
         }
 
+        public static int[,] CreateDiagonalSnakedReverseMatrix(int length, int firstNum)
+        {
+            int[,] array = new int[length, length];
+
+            var row = length - 1;
+            var column = length - 1;
+
+            for (int i = 0; i < length * length; i++)
+            {
+                array[row, column] = firstNum++;
+
+                if ((row + column) % 2 == 0)
+                {
+                    if (column > 0)
+                    {
+                        column--; 
+                        
+                        if (row < length - 1)
+                        {
+                            row++;
+                        }
+                    }
+                    else
+                    {
+                        row--;
+                    }
+                }
+                else
+                {
+                    if (row > 0)
+                    {
+                        row--;
+
+                        if (column < length - 1)
+                        {
+                            column++;
+                        }
+                    }
+                    else
+                    {
+                        column--;
+                    }
+                }
+            }
+
+            return array;
+        }
         public static int[,] CreateSpiralMatrix(int rowCount, int columnCount, int firstNum)
         {
             int[,] array = new int[rowCount, columnCount];
@@ -121,6 +192,43 @@ namespace Task1
             return array;
         }
 
+        public static int[,] CreateSpiralReverseMatrix(int rowCount, int columnCount, int firstNum)
+        {
+            int[,] array = new int[rowCount, columnCount];
+
+            int row = rowCount - 1, column = columnCount - 1;
+
+            int maxColumn = columnCount - 1;
+            int maxRow = rowCount - 1;
+            int min = 0;
+
+            for (int i = 1; i <= rowCount * columnCount; i++)
+            {
+                array[row, column] = firstNum;
+
+                if (row == min && column != maxColumn)
+                    column++;
+                else if (column == min)
+                    row--;
+                else if (row == maxRow)
+                    column--;
+                else if (column == maxColumn && row != min + 1)
+                    row++;                
+                
+                else
+                {
+                    maxColumn -= 1;
+                    maxRow -= 1;
+                    min += 1;
+                    row++;
+                }
+
+                firstNum++;
+            }
+
+            return array;
+        }
+
         public static void DisplayMatrix(this int[,] array)
         {
             var rowLength = array.GetLength(0);
@@ -135,6 +243,8 @@ namespace Task1
 
                 Console.WriteLine();
             }
+
+            Console.WriteLine();
         }
     }
 }
--- Homework_2_ШуленкоЄлизаветаРоманівна/HW2/Task1/Program.cs
+++ Homework_2_ШуленкоЄлизаветаРоманівна/HW2/Task1/Program.cs
@@ -8,11 +8,23 @@ class Program
         int[,] snakeMatrix = Matrix.CreateSnakedMatrix(3, 4, 1);
         snakeMatrix.DisplayMatrix();
 
+        int[,] snakeMatrixReverse = Matrix.CreateSnakedMatrixReverse(3, 4, 1);
+        snakeMatrixReverse.DisplayMatrix();
+
+        Console.WriteLine(new string('=',50));
+
         int[,] diagonalMatrix = Matrix.CreateDiagonalSnakedMatrix(4, 1);
         diagonalMatrix.DisplayMatrix();
 
-        int[,] spiralMatrix = Matrix.CreateSpiralMatrix(5, 6, 1);
+        int[,] diagonaReverselMatrix = Matrix.CreateDiagonalSnakedReverseMatrix(4, 1);
+        diagonaReverselMatrix.DisplayMatrix();
+
+        Console.WriteLine(new string('=', 50));
+
+        int[,] spiralMatrix = Matrix.CreateSpiralMatrix(6, 7, 1);
         spiralMatrix.DisplayMatrix();
 
+        int[,] spiralReverseMatrix = Matrix.CreateSpiralReverseMatrix(6, 7, 1);
+        spiralReverseMatrix.DisplayMatrix();
     }
 }
